8e867c52cb2f10a111836bef4893a2e68b9a9f4b
huangxiangkai <huangxiangkai@baidu.com>
2019-01-20 16:10:36 +0800

    Leetcode practice

---
+++ leetcode/Medium 151. Reverse Words in a String.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+
+"""
+This module is provided by
+	Authors: hxk11111
+Date:	2019/1/20
+File:   Medium 151. Reverse Words in a String.py
+"""
+'''
+Given an input string, reverse the string word by word.
+
+Example:  
+Input: "the sky is blue",
+Output: "blue is sky the".
+
+Note:
+A word is defined as a sequence of non-space characters.
+Input string may contain leading or trailing spaces. However, 
+your reversed string should not contain leading or trailing spaces.
+You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+Follow up: For C programmers, try to solve it in-place in O(1) space.
+'''
+
+
+class Solution(object):
+    def reverse(self, word):
+        return word[::-1]
+
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ""
+        s_reverse = []
+        start = 0
+        end = 0
+        while end < len(s):
+            if s[end] == " " and s[start: end].strip():
+                word = self.reverse(s[start: end].strip())
+                s_reverse.append(word)
+                start = end + 1
+            end += 1
+        if start != end and s[start: end].strip():
+            s_reverse.append(self.reverse(s[start: end].strip()))
+        return self.reverse(" ".join(s_reverse)) if s_reverse else ""
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.reverseWords(" 1")
---
+++ leetcode/Medium 152. Maximum Product Subarray.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+
+"""
+This module is provided by
+	Authors: hxk11111
+Date:	2019/1/20
+File:   Medium 152. Maximum Product Subarray.py
+"""
+'''
+Given an integer array nums, find the contiguous subarray within an array 
+(containing at least one number) which has the largest product.
+
+Example 1:
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+
+Example 2:
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+'''
+
+
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return
+        max_result = largest = least = nums[0]
+        for num in nums[1:]:
+            largest, least = max(largest * num, least * num, num), min(largest * num, least * num, num)
+            max_result = max(max_result, largest)
+        return max_result
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.maxProduct([0, 2])
